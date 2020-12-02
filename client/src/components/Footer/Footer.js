00f3cedca5c178e9a035df26012bdc8ad189f6c1
anudeepayilalath <anpds816@gmail.com>
2020-12-02 20:55:36 +0100

    footer fixed

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,40 +1,42 @@
 import React from 'react';
 import SocialMedia from '../SocialMedia/SocialMedia';
 
+
+import './Footer.css';
+ 
+
 const Footer = ()=>{
 
-  return (
-  
-          <div className="container my-4">
-            <div className="footer bg-info row justify-content-center">
-              <div id="firstItem">
-            <div className="footer row justify-content-center">
-              <div  className="firstItem col-lg-4 col-sm-12 col-md-6">
-                  <h2>Psicologxs de calle</h2>
-                  <h3>Carrer de la Rieretra, 24, 08001, Barcelona</h3>
-                  <h3>conatacto@psicologxsdecalle.com</h3>
-                  <h3>+34 000 00 00 00</h3>
-  
-              </div>
+return (
+    
+        <div className="container my-4">
+          <div className="footer row justify-content-center">
 
-              <div  className="firstItem col-lg-4 col-sm-12 col-md-6">
-                <h3>Colaboradores</h3>
-               {/* <div className="midcomp" > */}
-                <img className="footerImg" src="https://static1.squarespace.com/static/54c15c2ae4b0bad93722c9cb/t/56df438cb654f95102099987/1552503652342/?format=750w" width="60" />
-                <img className="footerImg" src="https://lh3.googleusercontent.com/proxy/gyZCnVBUZEVkjzWuUZqe_PrHWguAy406DzXXo42LnVufAsd2YJXfmJX5kz0yIs-5UneZSp8i58vKKg6EZtNtbeDZOI4iC40g6y3aC9tUZenxbt0sl6938MDHDQ5-I4v5bOxhkqxO" width="60"/>
+            <div className="firstItem col-lg-4 col-sm-12 col-md-6">
+                <h2>Psicólogxs de calle</h2>
+                <h4>Carrer de la Rieretra, 24, 08001, Barcelona</h4>
+                <h4>conatacto@psicologxsdecalle.com</h4>
+                <h4>+34 000 00 00 00</h4>
+           </div>
 
-                </div>
-                {/* </div> */}
-            </div>
-            <div  className="firstItem col-lg-4 col-sm-12 col-md-12"></div>
-            <SocialMedia/>
-                </div>
+           <div className="firstItem col-lg-4 col-sm-12 col-md-6">
+                  <h4>Colaboradores</h4>
+                  <img className="footerImg" src="https://static1.squarespace.com/static/54c15c2ae4b0bad93722c9cb/t/56df438cb654f95102099987/1552503652342/?format=750w" width="60" />
+                  <img className="footerImg" src="https://www.clipartmax.com/png/middle/279-2798685_png-logo-maker-online-real-clipart-and-vector-graphics-camera-logo-design.png" width="60"/>
+           </div>
 
-                </div>
+           <div className="firstItem col-lg-4 col-sm-12 col-md-12">
+                  <SocialMedia/>
+           </div>
 
+          </div> 
+        </div>
 
-</div> 
-  )
-}
+        
+            
+        
+)
 
-export default Footer;
+};
+
+export default Footer; 
--- client/src/components/SocialMedia/SocialMedia.js
+++ client/src/components/SocialMedia/SocialMedia.js
@@ -4,17 +4,18 @@ import './SocialMedia.css';
 const SocialMedia = () =>{
 
     return (
-        <>
-                <h3>Siguenos o comparte el proyecto</h3>
-                <div id="thirdItem">
+        <div>
+                <h4>Síguenos o comparte el proyecto</h4>
+                <div>
                     <i className="fab fa-instagram"></i>
-                
+                    <i className="fab fa-facebook-square"></i>
+                    <i className="fab fa-twitter"></i>
+                    <i className="fas fa-envelope"></i>
+                  
                 </div>
-                </>
+                </div>
+            )
 
-
-    )
-
-};
+    };
 
 export default SocialMedia;
