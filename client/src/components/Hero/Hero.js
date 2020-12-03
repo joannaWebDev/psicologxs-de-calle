6ab34546ac346fa2d888943aee6faca4120b1b0b
Sofia Carballo <sofia.carballo@live.com>
2020-12-03 18:20:30 +0100

    Add style, image and modify text for hero component

--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -1,22 +1,16 @@
 import React from 'react';
-
 import './hero.css'
-
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import Container from 'react-bootstrap/Container'
 
+
 const Hero = () => {
     return (
-
-      <Jumbotron fluid>
-        <Container>
-          <h1>Psicología de calle para personas sin hogar, por fin.</h1>
-          <p>
-            This is a modified jumbotron that occupies the entire horizontal space of
-            its parent.
-          </p>
-        </Container>
-      </Jumbotron>
+        <Jumbotron fluid>
+            <Container>
+            <h1>Psicología de calle<br/> para personas sin hogar, por fin.</h1>
+            </Container>
+        </Jumbotron>
     )
   }
   
