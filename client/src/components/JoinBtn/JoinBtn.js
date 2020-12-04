a901bddb9ace7d196377d683998b091b58098054
Sofia Carballo <sofia.carballo@live.com>
2020-12-04 12:23:57 +0100

    Add Join us button component and its style

---
+++ client/src/components/JoinBtn/JoinBtn.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './joinBtn.css'
+
+const JoinBtn = () => {
+    return (
+        <div className="join-us-btn text-uppercase text-center">
+            <a href="">¡Únete!</a>
+        </div>        
+    )
+}
+
+export default JoinBtn;
