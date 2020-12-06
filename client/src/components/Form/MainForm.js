46a38ca78b946b810991815d9042b0739176a295
Umit Can Batur <umitcan.batur@gmail.com>
2020-12-06 23:05:31 +0100

    textarea lengthened on form

--- client/src/components/Form/MainForm.js
+++ client/src/components/Form/MainForm.js
@@ -4,20 +4,20 @@ import Button from 'react-bootstrap/Button';
 
 const MainForm = () => {
     return (
-        <Form className = "form-group">
-            <Form.Group controlId="formName" className="col-sm-6 col-form-label" >
-                <Form.Label className="col-form-label-sm-1"> ¿Cómo te llamas? </Form.Label>
+        <Form className = "container centerForm">
+            <Form.Group controlId="formName" className="formInput" >
+                <Form.Label > ¿Cómo te llamas? </Form.Label>
                     <Form.Control type="text" placeholder="ej. María" />
             </Form.Group>
-            <Form.Group controlId="formEmail" className="col-sm-6 col-form-label">
+            <Form.Group controlId="formEmail" >
                 <Form.Label> ¿Cómo podemos contactar contigo? </Form.Label>
                     <Form.Control type="email" placeholder="ej. Maria12@gmail.com" />
             </Form.Group>
-            <Form.Group controlId="formComment" className="col-sm-6 col-form-label">
+            <Form.Group controlId="formComment" >
                 <Form.Label> ¿En qué podemos ayudarte?</Form.Label>
-                    <Form.Control type="textarea span6" rows="6" placeholder="Escribe tu mensaje aquí" />
+                    <Form.Control as="textarea" rows="4" placeholder="Escribe tu mensaje aquí" />
             </Form.Group>
-            <Button variant="primary" type="submit">
+            <Button variant="primary" type="submit" >
             Envía
             </Button>            
         </Form>
