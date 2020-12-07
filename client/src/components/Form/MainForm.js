989a8f86bc3a9a6a141819994b529570c9da31c3
Umit Can Batur <umitcan.batur@gmail.com>
2020-12-07 14:01:57 +0100

    Homepage form completed

--- client/src/components/Form/MainForm.js
+++ client/src/components/Form/MainForm.js
@@ -7,15 +7,19 @@ import './MainForm.css';
 const MainForm = () => {
     return (
         <Form className = "container centerForm">
-            <Form.Group controlId="formName" className="formInput" >
+            <div className ="formHeader">
+                <h1 className="marginTitle">Contáctanos</h1>
+                <h4 className="marginTitle">Y forma parte de la solución</h4>
+            </div>
+            <Form.Group controlId="formName" className="formInput marginInput" >
                 <Form.Label > ¿Cómo te llamas? </Form.Label>
                     <Form.Control type="text" placeholder="ej. María" />
             </Form.Group>
-            <Form.Group controlId="formEmail" className="formInput">
+            <Form.Group controlId="formEmail" className="formInput marginInput">
                 <Form.Label> ¿Cómo podemos contactar contigo? </Form.Label>
                     <Form.Control type="email" placeholder="ej. Maria12@gmail.com" />
             </Form.Group>
-            <Form.Group controlId="formComment" className="formInput">
+            <Form.Group controlId="formComment" className="formInput marginInput">
                 <Form.Label> ¿En qué podemos ayudarte?</Form.Label>
                     <Form.Control as="textarea" rows="4" placeholder="Escribe tu mensaje aquí" />
             </Form.Group>
