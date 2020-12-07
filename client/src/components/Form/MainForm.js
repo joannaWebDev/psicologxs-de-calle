8b8ca66d19332af94e70b40bdd6dd65769c58cc7
Umit Can Batur <umitcan.batur@gmail.com>
2020-12-07 13:18:04 +0100

    form aligned to center

--- client/src/components/Form/MainForm.js
+++ client/src/components/Form/MainForm.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
+import './MainForm.css';
+
 const MainForm = () => {
     return (
         <Form className = "container centerForm">
@@ -9,11 +11,11 @@ const MainForm = () => {
                 <Form.Label > ¿Cómo te llamas? </Form.Label>
                     <Form.Control type="text" placeholder="ej. María" />
             </Form.Group>
-            <Form.Group controlId="formEmail" >
+            <Form.Group controlId="formEmail" className="formInput">
                 <Form.Label> ¿Cómo podemos contactar contigo? </Form.Label>
                     <Form.Control type="email" placeholder="ej. Maria12@gmail.com" />
             </Form.Group>
-            <Form.Group controlId="formComment" >
+            <Form.Group controlId="formComment" className="formInput">
                 <Form.Label> ¿En qué podemos ayudarte?</Form.Label>
                     <Form.Control as="textarea" rows="4" placeholder="Escribe tu mensaje aquí" />
             </Form.Group>
