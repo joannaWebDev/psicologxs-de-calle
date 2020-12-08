3c70408c72a35f94ebfccc578f6e44906083d6f2
joannaWebDev <joannasmerea@gmail.com>
2020-12-08 09:58:45 +0100

    nothing to commit. just testing

--- client/src/components/Form/MainForm.js
+++ client/src/components/Form/MainForm.js
@@ -1,27 +1,35 @@
 import React from 'react';
-
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
+
+import './MainForm.css';
+
+
 const MainForm = () => {
     return (
-        <Form>
-            <Form.Group controlId="formName">
-                <Form.Label> ¿Cómo te llamas? </Form.Label>
-                    <Form.Control type="text" placeholder="Enter email" />
+        <Form className = "container centerForm">
+            <div className ="formHeader">
+                <h1 className="marginTitle">Contáctanos</h1>
+                <h4 className="marginTitle">Y forma parte de la solución</h4>
+            </div>
+            <Form.Group controlId="formName" className="formInput marginInput" >
+                <Form.Label > ¿Cómo te llamas? </Form.Label>
+                    <Form.Control type="text" placeholder="ej. María" />
             </Form.Group>
-            <Form.Group controlId="formEmail">
+            <Form.Group controlId="formEmail" className="formInput marginInput">
                 <Form.Label> ¿Cómo podemos contactar contigo? </Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control type="email" placeholder="ej. Maria12@gmail.com" />
             </Form.Group>
-            <Form.Group controlId="formComment">
+            <Form.Group controlId="formComment" className="formInput marginInput">
                 <Form.Label> ¿En qué podemos ayudarte?</Form.Label>
-                    <Form.Control type="textarea" placeholder="Enter email" />
+                    <Form.Control as="textarea" rows="4" placeholder="Escribe tu mensaje aquí" />
             </Form.Group>
-            <Button variant="primary" type="submit">
+            <Button className="join-us-btn" variant="primary" type="submit" >
             Envía
-            </Button>
+            </Button>          
         </Form>
+
     )
 }
 export default MainForm;
